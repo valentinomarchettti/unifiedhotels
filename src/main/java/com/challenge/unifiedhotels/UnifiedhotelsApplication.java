35ce59adc378b06fdfb9257c192221b799f48e75
valentinomarchettti <valentinomarchettti@gmail.com>
2026-01-13 11:46:16 -0300

    Initial commit

---
+++ src/main/java/com/challenge/unifiedhotels/UnifiedhotelsApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.unifiedhotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedhotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifiedhotelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/unifiedhotels/UnifiedhotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.unifiedhotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedhotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
